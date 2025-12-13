b3fa652bd52288d3c38708349081d904ce132b2c
alfa0805 <asadbekarslonbekov53@gmail.com>
2025-12-13 21:51:00 +0500

    ggg

--- script.js
+++ script.js
@@ -3,8 +3,6 @@ let menu = document.getElementsByClassName("menu")
 let openMenu = document.getElementById("openmenu")
 let closeMenu = document.getElementById("closemenu")
 
-
-
 openMenu.addEventListener("click" , function()  {
     menu[0].classList.add("toggel")
 })
@@ -12,3 +10,22 @@ openMenu.addEventListener("click" , function()  {
 closeMenu.addEventListener("click" , function()  {
     menu[0].classList.remove("toggel")
 })
+
+
+
+// let n=prompt("n sonini kiriting =");
+// let s=0;
+// for (let i=n; i<=n*2; i++){
+//     s+=i*i
+// }
+// console.log(s);
+
+// let a=+prompt("a sonini kiriting");
+// let b=+prompt("b sonini kiriting");
+// if (a<b){
+//     for (let i=a+1; i<b; i++){
+//         // console.log(i);
+//         for (let j=1; j<=i; j++)
+//         console.log(i);
+//         }
+// }
